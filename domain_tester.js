d1a9f438955bea4b000d6892d066474252687146
David Walz <davidjwalz@gmail.com>
2015-03-13 09:21:00 -0500

    Added an output log file for overtime the program runs.

--- domain_tester.js
+++ domain_tester.js
@@ -1,22 +1,33 @@
 var webPage = require('webpage');
+var fs = require('fs');
+var path = 'log.txt';
 var array_position = 0;
 var mydate = new Date();
 var now = mydate.valueOf();
 var startTime = now;
+var timeInSecs = 0;
 var successes = 0;
 var failures = 0;
 var scheme = 'http://www.';
-var arrayProdsites = [
-        "yahoo.com",
-        "wikipedia.org",
-        "reddit.com/r/raspberry_pi"
+var Prodsites = [
+        "dolce.com",
+        "surforsound.com",
+        "warwickhotels.com",
+        "silveradoresort.com",
+        "outerbanks.org",
+        "newmexico.org"
         ];
+
+console.log(" ");
+console.log("*** Domain Tester ***");
+console.log(" ");
+
 function open_website(url){
     // Open website
     var action = '\x1b[33mRedirected\x1b[0m: ';
 
     if (!url) {
-        url = scheme + arrayProdsites[array_position] + '/';
+        url = scheme + Prodsites[array_position] + '/';
         action = 'Opening: ';
     }
 
@@ -41,12 +52,14 @@ function open_website(url){
         // Render
         if(status === "success") {
             console.log(" Status: \x1b[32m" + status + '\x1b[0m');
+        console.log(" Page Title:  " + page.title);
             console.log('');
             successes++;
             //page.render(array_position + '_example.png');
         } else {
             console.log(' Status: \x1b[31mFailed\x1b[0m');
             console.log('');
+     // page.render(startTime + '.png');
             failures++;
         }
 
@@ -55,15 +68,23 @@ function open_website(url){
         page.close();
 
         // Go to the next or exit
-        if(arrayProdsites.length !== array_position){
+        if(Prodsites.length !== array_position){
             open_website();
         } else {
             console.log(successes + ' successes, ' + failures + ' failures');
             var endTime = new Date().valueOf();
             var timeDiff = endTime - startTime;
-            var timeInSecs = timeDiff/1000;
+            timeInSecs = timeDiff/1000;
+            var logcontent = [
+                    mydate + " ",
+                    timeInSecs + " seconds of runtime",
+                    Prodsites.length + " domains",
+                    successes + " total successes",
+                    failures + " total failures",
+                    ];
             console.log('Test suite took ' + timeInSecs + ' seconds to complete');
             console.log(' ');
+            fs.write(path, logcontent, 'a');
             phantom.exit();
         }
     });
